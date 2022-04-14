8a5a86b1d064d5648da239e9c82602d12ab4606e
Артем Томило <artemtomilo@MacBook-Air-Artem.local>
2022-04-14 20:13:45 +0300

    New class file

--- HomeWork-15/HomeWork-15/ViewController.swift
+++ HomeWork-15/HomeWork-15/ViewController.swift
@@ -64,36 +64,3 @@ class ViewController: UIViewController {
     }
 
 }
-//<<<<<<< HEAD
-//=======
-
-class SeconViewController: UIViewController {
-    
-    let secondButton = UIButton()
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-                
-        view.backgroundColor = .gray
-        
-        view.addSubview(secondButton)
-        
-        secondButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            secondButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            secondButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            secondButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
-        secondButton.frame.size = .init(width: 100, height: 50)
-        secondButton.addTarget(self, action: #selector(goFirstVC(_:)), for: .primaryActionTriggered)
-        secondButton.backgroundColor = .blue
-        secondButton.setTitle("My second Button", for: .normal)
-    }
-    
-    @objc func goFirstVC(_ sender: UIButton) {
-        dismiss(animated: true)
-    }
-}
-//>>>>>>> SecondVC
